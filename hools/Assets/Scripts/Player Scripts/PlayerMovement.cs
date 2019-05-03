7cfa8f6b0afc253a64b2d8bf57afef99f4c7e3c1
KubaBBB <jakubboliglowa1996@gmail.com>
2019-05-03 23:28:45 +0200

    Added movemet and mouse look

---
+++ hools/Assets/Scripts/Player Scripts/PlayerMovement.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private CharacterController _characterController;
+
+    private Vector3 _moveDirection;
+
+    private float _gravity = 9.81f;
+    private float _verticalVelocity;
+
+    public float speed = 5f;
+    public float jumpForce = 4;
+    
+    private void Awake ()
+    {
+        _characterController = GetComponent<CharacterController> ();
+    }
+
+	// Update is called once per frame
+	void Update () {
+        MoveThePlayer ();	
+	}
+
+    void MoveThePlayer ()
+    {
+        _moveDirection = new Vector3 ( Input.GetAxis ( Axis.HORIZONTAL ), 
+            0f,
+            Input.GetAxis ( Axis.VERTICAL ) );
+
+        _moveDirection = transform.TransformDirection ( _moveDirection );
+
+        _moveDirection *= speed*Time.deltaTime;
+
+        ApplyGravity ();
+
+        _characterController.Move ( _moveDirection );
+    }
+
+    void ApplyGravity ()
+    {
+        if ( _characterController.isGrounded )
+        {
+            _verticalVelocity -= _gravity * Time.deltaTime;
+
+            PlayerJump ();
+        }
+        else
+        {
+            _verticalVelocity -= _gravity * Time.deltaTime;
+        }
+
+        _moveDirection.y = _verticalVelocity * Time.deltaTime;
+    }
+
+    void PlayerJump ()
+    {
+        if(_characterController.isGrounded && Input.GetKeyDown ( KeyCode.Space ) )
+        {
+            _verticalVelocity = jumpForce;
+        }
+    }
+}
---
+++ hools/Assets/Scripts/Player Scripts/PlayerSprintAndCrouch.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+
+public class PlayerSprintAndCrouch : MonoBehaviour
+{
+    private PlayerMovement _playerMovement;
+
+    public float _sprintSpeed = 10f;
+    public float _moveSpeed = 5f;
+    public float _crouchSpeed = 2f;
+
+    private Transform _lookRoot;
+
+    private float _standHeight = 1.6f;
+    private float _crouchHeight = 1f;
+
+    private bool _isCrouching;
+
+    private void Awake ()
+    {
+        _playerMovement = GetComponent<PlayerMovement> ();
+        _lookRoot = transform.GetChild ( 0 );
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        Sprint ();
+        Crouch ();
+    }
+
+    void Sprint ()
+    {
+        if ( Input.GetKeyDown ( KeyCode.LeftShift ) && !_isCrouching )
+        {
+            _playerMovement.speed = _sprintSpeed;
+        }
+        else
+        {
+            _playerMovement.speed = _moveSpeed;
+        }
+
+    }
+
+    void Crouch ()
+    {
+        if ( Input.GetKeyDown ( KeyCode.C ) )
+        {
+            if ( _isCrouching )
+            {
+                _lookRoot.localPosition = new Vector3 ( 0f, _standHeight, 0f );
+                _playerMovement.speed = _moveSpeed;
+
+                _isCrouching = false;
+            }
+            else
+            {
+                _lookRoot.localPosition = new Vector3 ( 0f, _crouchHeight, 0f );
+                _playerMovement.speed = _crouchSpeed;
+
+                _isCrouching = true;
+            }
+        }
+    }
+}
