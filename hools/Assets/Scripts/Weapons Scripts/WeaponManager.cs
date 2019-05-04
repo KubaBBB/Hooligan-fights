1b8a245226957102b9b2e22f81f04a4f230d6bb4
KubaBBB <jakubboliglowa1996@gmail.com>
2019-05-04 15:40:27 +0200

    Added weapons change

--- hools/Assets/Scripts/Weapons Scripts/WeaponManager.cs
+++ hools/Assets/Scripts/Weapons Scripts/WeaponManager.cs
@@ -4,13 +4,47 @@ using UnityEngine;
 
 public class WeaponManager : MonoBehaviour {
 
+    [SerializeField]
+    private WeaponHandler [] _weaponHandlers;
+
+    private int _currentWeapoinIndex;
+
+
 	// Use this for initialization
 	void Start () {
-		
+        _currentWeapoinIndex = 0;
+        _weaponHandlers [_currentWeapoinIndex].gameObject.SetActive ( true );
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        if ( Input.GetKeyDown ( KeyCode.Alpha1 ) )
+        {
+            TurnOnSelectedWeapon ( 0 ); //Axe
+        }
+
+        if ( Input.GetKeyDown ( KeyCode.Alpha2 ) )
+        {
+            TurnOnSelectedWeapon ( 1 ); //Rev
+        }
+
+        if ( Input.GetKeyDown ( KeyCode.Alpha3 ) )
+        {
+            TurnOnSelectedWeapon ( 2 ); //assault rif
+        }
+    }
+
+    void TurnOnSelectedWeapon(int weaponIndex )
+    {
+        _weaponHandlers [_currentWeapoinIndex].gameObject.SetActive ( false );
+
+        _weaponHandlers [weaponIndex].gameObject.SetActive ( true );
+
+        _currentWeapoinIndex = weaponIndex;
+    }
+
+    public WeaponHandler GetCurrentWeaponHandler()
+    {
+        return _weaponHandlers [_currentWeapoinIndex];
+    }
 }
